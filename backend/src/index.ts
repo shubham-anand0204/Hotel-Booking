4562e5848ecfa874afb0f6f543f45949279b5e09
Shubham Anand <shubham1382.be22@chitkarauniversity.edu.in>
2025-03-07 17:06:15 +0530

    Added my Folder

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ mesage: "hello from express endpoint!" });
+});
+
+
+app.listen(5000,()=>{
+    console.log("Server is running at port 5000");
+})
