d6f166b2b8c8b00141213ab6263db3811a4e2c28
Shubham Anand <shubham1382.be22@chitkarauniversity.edu.in>
2025-03-08 10:24:59 +0530

    Update Header component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+  ],
 })
+
